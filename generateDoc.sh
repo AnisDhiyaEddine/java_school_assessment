4415895f1a30d5cad63647bca80a1e8a17824c25
anisdhiyaeddine <dhiaeboudiaf@gmail.com>
2022-01-09 22:54:29 +0100

    quick fix

--- generateDoc.sh
+++ generateDoc.sh
@@ -1,6 +1,6 @@
 rm -r ./javadoc/*
 
-javadoc "./src/Main.java" "./src/classes/Arbre.java" "./src/classes/Personne.java" "./src/classes/Membre.java"  "./src/classes/President.java" "./src/classes/Association.java"  -d ./javadoc src.classes src.utiles;
+javadoc "./src/Main.java" "./src/classes/Arbre.java" "./src/classes/Personne.java" "./src/classes/Membre.java"  "./src/classes/President.java" "./src/classes/Association.java"  -d ./doc src.classes src.utiles;
 
 echo ----------------------------------------------------
 echo "Javadoc generated in ./javadoc/"
